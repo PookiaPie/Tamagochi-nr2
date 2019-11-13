678caf332b77e1346a239263a2c23bc3e566dd7f
Pookia <54947020+PookiaPie@users.noreply.github.com>
2019-11-13 14:26:14 +0100

    Add project files.

---
+++ Tamagochi nr2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagochi_nr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
